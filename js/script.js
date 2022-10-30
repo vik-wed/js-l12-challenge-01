f671bf74f3df52db7a9628de71be043b82f7e518
vik-wed <wedel.viktoria@gmail.com>
2022-10-29 20:46:16 -0400

    Added funct to button to generate random img

--- js/script.js
+++ js/script.js
@@ -6,7 +6,27 @@ const img = document.querySelector(".img");
 async function getImage () {
     let res = await fetch("https://picsum.photos/v2/list?limit=100");
     let images = await res.json();
+    selectRandomImage(images);
     console.log(images);
 }
 
-getImage();
+
+async function selectRandomImage(images){
+    let randomIndex = Math.floor(Math.random()*images.length);
+    let randomImage = images[randomIndex];
+    displayImage(randomImage);
+    
+}
+
+async function displayImage(randomImage){
+    let author = randomImage.author;
+    let imageAddress = randomImage.download_url; 
+    authorSpan.innerText = author;
+    img.src = imageAddress;
+    imgDiv.classList.remove("hide");
+}
+
+button.addEventListener("click", function(){
+    getImage();
+})
+
